62bdc03bd1dce5b353e631836ac97cc8ccfec4b8
XiaotongZhao <275451183@qq.com>
2022-11-23 17:39:37 +0800

    add env variables

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Introduction 
 Front project based on React
+
+React set env variables, please refer to https://create-react-app.dev/docs/adding-custom-environment-variables/
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,58 +3,55 @@ import { Space, Table, Tag } from 'antd';
 const { Content } = Layout;
 
 function Home() {
-    const dataSource = [
-        {
-          key: '1',
-          name: 'Mike',
-          age: 32,
-          address: '10 Downing Street',
-        },
-        {
-          key: '2',
-          name: 'John',
-          age: 42,
-          address: '10 Downing Street',
-        },
-      ];
-      
-      const columns = [
-        {
-          title: 'Name',
-          dataIndex: 'name',
-          key: 'name',
-        },
-        {
-          title: 'Age',
-          dataIndex: 'age',
-          key: 'age',
-        },
-        {
-          title: 'Address',
-          dataIndex: 'address',
-          key: 'address',
-        },
-      ];
+  const dataSource = [
+    {
+      key: '1',
+      name: 'Mike',
+      age: 32,
+      address: '10 Downing Street',
+    },
+    {
+      key: '2',
+      name: 'John',
+      age: 42,
+      address: '10 Downing Street',
+    },
+  ];
 
-      let test = process.env.API_BASE_URL;
-      debugger
-    return (
-        <>
-         <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          </Breadcrumb>
-          <Content
-           className="site-layout-background"
-           style={{
-            padding: 24,
-            margin: 0,
-            minHeight: 768,
-           }}>
-            {process.env.API_BASE_URL}
-          <Table dataSource={dataSource} columns={columns} />;
-          </Content>
-        </>
-    );
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+  ];
+
+  return (
+    <>
+      <Breadcrumb style={{ margin: '16px 0' }}>
+        <Breadcrumb.Item>Home page {process.env.REACT_APP_API_BASE_URL}</Breadcrumb.Item>
+      </Breadcrumb>
+      <Content
+        className="site-layout-background"
+        style={{
+          padding: 24,
+          margin: 0,
+          minHeight: 768,
+        }}>
+        <Table dataSource={dataSource} columns={columns} />;
+      </Content>
+    </>
+  );
 }
 
 export default Home;
