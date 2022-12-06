76c73ff19a61644ee258c9b4fad65ce2011b7a37
XiaotongZhao <275451183@qq.com>
2022-12-06 16:21:32 +0800

    add echart graph

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,97 +1,140 @@
 import React from 'react';
-import { Button, Form, Input, Select } from 'antd';
-import type { FormInstance } from 'antd/es/form';
+import ReactECharts from 'echarts-for-react';
 
-const { Option } = Select;
-
-const layout = {
-  labelCol: { span: 8 },
-  wrapperCol: { span: 16 },
-};
-const tailLayout = {
-  wrapperCol: { offset: 8, span: 16 },
-};
-
-class Test extends React.Component {
-  formRef = React.createRef<FormInstance>();
-
-  onGenderChange = (value: string) => {
-    switch (value) {
-      case 'male':
-        this.formRef.current!.setFieldsValue({ note: 'Hi, man!' });
-        return;
-      case 'female':
-        this.formRef.current!.setFieldsValue({ note: 'Hi, lady!' });
-        return;
-      case 'other':
-        this.formRef.current!.setFieldsValue({ note: 'Hi there!' });
-        break;
-      default:
-    }
-  };
-
-  onFinish = (values: any) => {
-    console.log(values);
-  };
-
-  onReset = () => {
-    this.formRef.current!.resetFields();
-  };
-
-  onFill = () => {
-    this.formRef.current!.setFieldsValue({
-      note: 'Hello world!',
-      gender: 'male',
-    });
-  };
-
-  render() {
-    return (
-      <Form {...layout} ref={this.formRef} name="control-ref" onFinish={this.onFinish}>
-        <Form.Item name="note" label="Note" rules={[{ required: true }]}>
-          <Input />
-        </Form.Item>
-        <Form.Item name="gender" label="Gender" rules={[{ required: true }]}>
-          <Select
-            placeholder="Select a option and change input text above"
-            onChange={this.onGenderChange}
-            allowClear
-          >
-            <Option value="male">male</Option>
-            <Option value="female">female</Option>
-            <Option value="other">other</Option>
-          </Select>
-        </Form.Item>
-        <Form.Item
-          noStyle
-          shouldUpdate={(prevValues, currentValues) => prevValues.gender !== currentValues.gender}
-        >
-          {({ getFieldValue }) =>
-            getFieldValue('gender') === 'other' ? (
-              <Form.Item
-                name="customizeGender"
-                label="Customize Gender"
-                rules={[{ required: true }]}
-              >
-                <Input />
-              </Form.Item>
-            ) : null
-          }
-        </Form.Item>
-        <Form.Item {...tailLayout}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-          <Button htmlType="button" onClick={this.onReset}>
-            Reset
-          </Button>
-          <Button type="link" htmlType="button" onClick={this.onFill}>
-            Fill form
-          </Button>
-        </Form.Item>
-      </Form>
-    );
+const Test: React.FC = () => {
+  const nodeClick = (event: any) => {
+    alert(`node value is ${event.value}`)
   }
-}
+
+  const onChartLegendselectchanged = (event: any) => {
+    alert("trigger onChartLegendselectchanged")
+
+  }
+
+  var graph = {
+    nodes: [
+      {
+        id: '0',
+        name: '20',
+        value: 20,
+        symbolSize: 50,
+        x: 300,
+        y: 240,
+        category: 3
+      },
+      {
+        id: '1',
+        name: '10',
+        value: 10,
+        symbolSize: 50,
+        x: 350,
+        y: 300,
+        category: 0
+      },
+      {
+        id: '2',
+        name: '30',
+        value: 30,
+        symbolSize: 50,
+        x: 250,
+        y: 300,
+        category: 0
+      },
+      {
+        id: '3',
+        name: '15',
+        value: 15,
+        symbolSize: 50,
+        x: 300,
+        y: 350,
+        category: 3
+      },
+      {
+        id: '4',
+        name: '17',
+        value: 17,
+        symbolSize: 50,
+        x: 200,
+        y: 350,
+        category: 3
+      }
+    ],
+    links: [
+      {
+        source: '0',
+        target: '1'
+      },
+      {
+        source: '0',
+        target: '2'
+      },
+      {
+        source: '2',
+        target: '3'
+      },
+      {
+        source: '2',
+        target: '4'
+      }
+    ],
+    categories: [
+      {
+        name: 'A'
+      },
+      {
+        name: 'B'
+      },
+      {
+        name: 'C'
+      },
+      {
+        name: 'D'
+      }
+    ]
+  };
+
+  const onEvents = {
+    'click': nodeClick,
+    'legendselectchanged': onChartLegendselectchanged
+  }
+
+  const options = {
+    tooltip: {},
+    legend: [
+      {
+        data: graph.categories.map(function (a) {
+          return a.name;
+        })
+      }
+    ],
+    series: [
+      {
+        type: 'graph',
+        layout: 'none',
+        data: graph.nodes,
+        links: graph.links,
+        categories: graph.categories,
+        roam: true,
+        label: {
+          show: true,
+          position: 'right',
+          formatter: '{b}'
+        },
+        labelLayout: {
+          hideOverlap: true
+        },
+        scaleLimit: {
+          min: 1,
+          max: 1
+        },
+        lineStyle: {
+          color: 'source'
+        }
+      }
+    ]
+  };
+
+  return <ReactECharts option={options} style={{ height: '800px', width: '100%' }} onEvents={onEvents} />;
+};
 
 export default Test;
